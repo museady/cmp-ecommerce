57ef6e112227088c67f02031c79db33f1566e60d
Museady <muhammad2ali.ismail@gmail.com>
2024-12-14 13:18:21 +0200

    Update AppIcons: Switch to Outlined icons for consistency

--- composeApp/src/commonMain/kotlin/com/museady/cmp/ecommerce/designsystem/icons/AppIcons.kt
+++ composeApp/src/commonMain/kotlin/com/museady/cmp/ecommerce/designsystem/icons/AppIcons.kt
@@ -2,22 +2,25 @@ package com.museady.cmp.ecommerce.designsystem.icons
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material.icons.filled.Earbuds
 import androidx.compose.material.icons.filled.Headphones
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.filled.Speaker
+import androidx.compose.material.icons.outlined.Earbuds
+import androidx.compose.material.icons.outlined.Headphones
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Speaker
 
-object AppIcons{
+object AppIcons {
     val Add = Icons.Default.Add
     val Remove = Icons.Default.Remove
-    val Home = Icons.Default.Home
+    val Home = Icons.Outlined.Home
     val HomeFilled = Icons.Filled.Home
-    val Speakers = Icons.Default.Speaker
+    val Speakers = Icons.Outlined.Speaker
     val SpeakerFilled = Icons.Filled.Speaker
-    val Headphones = Icons.Default.Headphones
+    val Headphones = Icons.Outlined.Headphones
     val HeadphonesFilled = Icons.Filled.Headphones
-    val Earphone = Icons.Default.Earbuds
+    val Earphone = Icons.Outlined.Earbuds
     val EarphoneFilled = Icons.Filled.Earbuds
 }
